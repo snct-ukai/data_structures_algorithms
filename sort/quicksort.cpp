adffb6ec4c377325a8f4ed5251d7e7043d0a2956
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-04-28 11:44:05 +0900

    update

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -5,33 +5,33 @@ using namespace std;
 
 static int cnt1 = 0, cnt2 = 0;
 
-void QuickSort(vector<int> S, int B, int T)
+void QuickSort(vector<int>* S, int B, int T)
 {
 	if(B>=T)
 		return;
 
-	int div = S[B];
+	int div = (*S)[B];
 
 	int i = B;
 	int j = T;
 	while(i<j){
-		while(i<=j && S[i]<=div){
+		while(i<=j && (*S)[i]<=div){
 			i++;
 		}
-		while(i<=j && S[j]>div){
+		while(i<=j && (*S)[j]>div){
 			j--;
 		}
 
 		if(i<j){
-			int tmp = S[i];
-			S[i] = S[j];
-			S[j] = tmp;
+			int tmp = (*S)[i];
+			(*S)[i] = (*S)[j];
+			(*S)[j] = tmp;
 		}
 	}
 
-	int tmp = S[B];
-	S[B] = S[j];
-	S[j] = tmp;
+	int tmp = (*S)[B];
+	(*S)[B] = (*S)[j];
+	(*S)[j] = tmp;
 
 	QuickSort(S, B, j-1);
 	QuickSort(S, j+1, T);
@@ -55,7 +55,7 @@ int main(int argc, char* argv[])
 		S.push_back(s);
 	}
 
-	QuickSort(S, 0, datanum - 1);
+	QuickSort(&S, 0, datanum - 1);
 	for(auto s : S){
 		cout << s << " ";
 	}
