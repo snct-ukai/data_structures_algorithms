93bb02cab1780285c1340fc045f84cf899720ea0
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-04-28 11:52:54 +0900

    update

--- sort/bubblesort.cpp
+++ sort/bubblesort.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 static int cnt1 = 0, cnt2 = 0;
 
-void BubbleSort(vector<int> S, int N){
+void BubbleSort(vector<int>* S, int N){
   bool flag;
   do{
     flag = false;
     for(int i = 0; i < N - 1; i++){
-      if(cnt1++, S[i] > S[i + 1]){
+      if(cnt1++, (*S)[i] > (*S)[i + 1]){
         cnt2++;
-        int tmp = S[i];
-        S[i] = S[i + 1];
-        S[i + 1] = tmp;
+        int tmp = (*S)[i];
+        (*S)[i] = (*S)[i + 1];
+        (*S)[i + 1] = tmp;
 
         flag = true;
       }
@@ -43,7 +43,7 @@ int main(int argc, char* argv[])
 		S.push_back(s);
 	}
 
-	BubbleSort(S, 10);
+	BubbleSort(&S, 10);
 
   cout << "bubbleSort:" << cnt1 << ":" << cnt2 << endl;
 	return 0;
